e50c08f4d62b03afe165879b55f4f38b454475e8
salo <562441514@qq.com>
2019-07-19 11:54:55 +0800

    三个详情页的大概模样

--- src/view/student/detail.vue
+++ src/view/student/detail.vue
@@ -1,3 +1,212 @@
 <template>
-  <div>student detail</div>
+  <page-view
+    title="学生信息"
+    logo="https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png"
+  >
+    <detail-list slot="headerContent" size="small" :col="3" class="detail-layout">
+      <detail-list-item term="姓名">谁谁谁</detail-list-item>
+      <detail-list-item term="性别">F/M</detail-list-item>
+      <detail-list-item term="入学年份">时间</detail-list-item>
+      <detail-list-item term="专业">软件工程</detail-list-item>
+    </detail-list>
+
+    <a-card style="margin-top: 24px" :bordered="false" title="详细信息">
+      <a-form-item :wrapperCol="{ span: 24 }" style="text-align: right">
+        <a-button htmlType="submit" @click="handleEdit(record)">
+          <a-icon type="edit" />
+        </a-button>
+      </a-form-item>
+      <detail-list>
+        <detail-list-item term="手机号码">11位</detail-list-item>
+        <detail-list-item term="emial">email</detail-list-item>
+        <detail-list-item term="备注">除了编辑按钮学生本人可见，其余所有人可见</detail-list-item>
+      </detail-list>
+      <a-divider style="margin-bottom: 32px" />
+      <a-form-item label="个人简介">
+        <a-textarea contenteditable="false" rows="10" readonly value="我是简介" style="border:none" />
+      </a-form-item>
+      <a-divider style="margin-bottom: 32px" />
+      <a-form-item label="实训经历">
+        <a-textarea contenteditable="false" rows="10" readonly value="我是简介" style="border:none" />
+      </a-form-item>
+
+      <a-modal title="编辑信息" :width="800" v-model="visible" @ok="handleOk">
+        <a-form-item label="手机号码">
+          <a-input
+            v-decorator="[
+            'name',
+            {rules: [{ required: true, message: '必须填写手机号码' }]}
+          ]"
+            name="name"
+            placeholder="手机号码"
+          />
+        </a-form-item>
+        <a-form-item label="email">
+          <a-input
+            v-decorator="[
+            'name',
+            {rules: [{ required: true, message: '必须填写email' }]}
+          ]"
+            name="name"
+            placeholder="email"
+          />
+        </a-form-item>
+
+        <a-form-item label="个人简介">
+          <a-textarea
+            rows="10"
+            v-decorator="[
+            'name',
+            {rules: [{ required: true, message: '必须填写个人简介' }]}
+          ]"
+            name="name"
+            placeholder="这里填写个人简介"
+          />
+        </a-form-item>
+
+        <a-form-item label="实训经历">
+          <a-textarea
+            rows="10"
+            v-decorator="[
+            'name',
+            {rules: [{ required: true, message: '必须填写实训经历' }]}
+          ]"
+            name="name"
+            placeholder="这里填写实训经历"
+          />
+        </a-form-item>
+      </a-modal>
+    </a-card>
+    <a-card style="margin-top: 24px" :bordered="false" title="学生作品">
+      <a-form-item :wrapperCol="{ span: 24 }" style="text-align: right">
+        <a-button htmlType="submit" @click="handleUpdateEdit(record)">
+          <a-icon type="edit" />
+        </a-button>
+      </a-form-item>
+      <detail-list>
+        <detail-list-item term="学生作品名">
+          <a>url</a>
+        </detail-list-item>
+        <detail-list-item term="学生作品名">
+          <a>url</a>
+        </detail-list-item>
+        <detail-list-item term="学生作品名">
+          <a>url</a>
+        </detail-list-item>
+      </detail-list>
+
+      <a-modal title="编辑信息" :width="800" v-model="updateVisible" @ok="handleOk">
+        <a-upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          :defaultFileList="defaultFileList"
+        >
+          <a-button>
+            <a-icon type="upload" />Upload
+          </a-button>
+        </a-upload>
+      </a-modal>
+    </a-card>
+  </page-view>
 </template>
+
+<script>
+import { mixinDevice } from "@/utils/mixin";
+import { PageView } from "@/layouts";
+import DetailList from "@/components/tools/DetailList";
+
+const DetailListItem = DetailList.Item;
+
+export default {
+  name: "Advanced",
+  components: {
+    PageView,
+    DetailList,
+    DetailListItem
+  },
+  mixins: [mixinDevice],
+  data() {
+    return {
+      defaultFileList: [
+        {
+          uid: "1",
+          name: "xxx.png",
+          status: "done",
+          response: "Server Error 500", // custom error message to show
+          url: "http://www.baidu.com/xxx.png"
+        },
+        {
+          uid: "2",
+          name: "yyy.png",
+          status: "done",
+          url: "http://www.baidu.com/yyy.png"
+        },
+        {
+          uid: "3",
+          name: "zzz.png",
+          status: "error",
+          response: "Server Error 500", // custom error message to show
+          url: "http://www.baidu.com/zzz.png"
+        }
+      ],
+      updateVisible: false,
+      visible: false,
+      mdl: {}
+    };
+  },
+  methods: {
+    handleChange({ file, fileList }) {
+      if (file.status !== "uploading") {
+        console.log(file, fileList);
+      }
+    },
+    handleEdit(record) {
+      this.mdl = Object.assign({}, record);
+      this.visible = true;
+    },
+    handleUpdateEdit(record) {
+      this.mdl = Object.assign({}, record);
+      this.updateVisible = true;
+    },
+    handleOk() {}
+  }
+};
+</script>
+
+<style scoped>
+.detail-layout {
+  margin-left: 44px;
+}
+.text {
+  color: rgba(0, 0, 0, 0.45);
+}
+
+.heading {
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 20px;
+}
+
+.no-data {
+  color: rgba(0, 0, 0, 0.25);
+  text-align: center;
+  line-height: 64px;
+  font-size: 16px;
+
+  i {
+    font-size: 24px;
+    margin-right: 16px;
+    position: relative;
+    top: 3px;
+  }
+}
+
+.mobile {
+  .detail-layout {
+    margin-left: unset;
+  }
+  .text {
+  }
+  .status-list {
+    text-align: left;
+  }
+}
+</style>
