5de4ff70fcaa777689bb29a050802e21f726ef09
Chuanjian Li <chuanjianli@outlook.com>
2019-07-28 09:19:50 +0800

    feat: internship list

--- src/view/internship/list/queryList.vue
+++ src/view/internship/list/queryList.vue
@@ -26,17 +26,23 @@
           </a-row>
         </a-form>
       </div>
-      <a-table :columns="enterpriseColumns" :dataSource="internships">
+      <a-table :columns="columnsSelector()" :dataSource="internships" rowKey="_id">
         <!-- <a slot="name" slot-scope="text" href="javascript:;">{{text}}</a> -->
         <span slot="serial" slot-scope="text, record, index">{{ index + 1 }}</span>
         <span slot="qualificate_file">
           <a href="#">filestitle</a>
         </span>
+        <!-- <span slot="description" slot-scope="text">
+
+        </span> -->
+
         <span slot="status" slot-scope="text">
           <a-badge :status="text | statusTypeFilter" :text="text | statusFilter" />
         </span>
-        <span slot="action">
-          <a href="#">查看</a>
+        <span slot="action" slot-scope="text,record">
+          <template>
+            <a slot="action" @click="goToExpDetail(record)" >查看</a>
+          </template>
         </span>
       </a-table>
     </a-card>
@@ -45,6 +51,7 @@
 <script>
 /* import moment from 'moment'
 import { getRoleList, getServiceList } from '@/api/manage' */
+import store from '@/store'
 import { getInternshipList } from '@/api/enterprise'
 const statusMap = {
   0: {
@@ -109,15 +116,59 @@ const enterpriseColumns = [
     scopedSlots: { customRender: 'action' }
   }
 ]
+const studentColumns = [
+  {
+    title: '#',
+    scopedSlots: { customRender: 'serial' }
+  },
+  { title: '公司' },
+  {
+    title: '实训题目',
+    dataIndex: 'topic'
+  },
+  {
+    title: '实训开始日期',
+    dataIndex: 'exp_begin_time'
+  },
+  {
+    title: '实训结束日期',
+    dataIndex: 'exp_end_time'
+  },
+  {
+    title: '提交日期',
+    dataIndex: 'submit_time'
+  },
+  {
+    title: '实训描述',
+    dataIndex: 'description',
+    scopedSlots: { customRender: 'description' }
+  },
+  {
+    title: '意向人数',
+    dataIndex: 'need_num'
+  },
+  {
+    title: '申请截至日期',
+    dataIndex: 'apply_end_time'
+  },
+
+  {
+    title: '操作',
+    dataIndex: 'action',
+    scopedSlots: { customRender: 'action' }
+  }
+]
 export default {
   name: 'ExpTableList',
   data () {
     return {
       enterpriseColumns,
+      studentColumns,
       mdl: {},
       queryParam: {},
       loading: false,
-      internships: []
+      internships: [],
+      role: ''
     }
   },
   mounted () {
@@ -133,14 +184,26 @@ export default {
       return statusMap[type].status
     }
   },
-  methods: {
-    Searchlist () {
-      this.loading = true
-      setTimeout(() => {
-        this.data = data
-        this.loading = false
-      }, 300)
-    }
+  methods: { goToExpDetail (e) {
+    const _this = this
+    console.log(e)
+    store.commit('SET_EXP_ID', e._id)
+    _this.$router.push({ name: 'internshipdetail' })
+  },
+  columnsSelector () {
+    this.role = store.getters.role
+    if (this.role === 'enterprise') {
+      return this.enterpriseColumns
+    } else if (this.role === 'student') {
+      return this.studentColumns
+    } else { return this.adminColums }
+  },
+  Searchlist () {
+    this.loading = true
+    setTimeout(() => {
+      this.loading = false
+    }, 300)
+  }
   }
 }
 </script>
