ea34f12a0e7992322dbe20b9bac2a631ad370a62
salo <562441514@qq.com>
2019-07-29 14:10:06 +0800

    fix：对项目列表做了权限

--- src/view/internship/list/queryList.vue
+++ src/view/internship/list/queryList.vue
@@ -32,20 +32,18 @@
         <span slot="qualificate_file">
           <a href="#">filestitle</a>
         </span>
-        <span slot="description" slot-scope="text">
-          {{text|ellipsis}}
-        </span>
+        <span slot="description" slot-scope="text">{{text|ellipsis}}</span>
 
         <span slot="status" slot-scope="text">
           <a-badge :status="text | statusTypeFilter" :text="text | statusFilter" />
         </span>
         <span slot="action" slot-scope="text,record">
           <template>
-            <a slot="action" @click="goToExpDetail(record)" >查看</a>
-            <a-divider type="vertical" />
-            <a @click="goToAppList(record)">查看申请</a>
-            <a-divider type="vertical" />
-            <a @click="goToStudentList(record)">学生管理</a>
+            <a slot="action" @click="goToExpDetail(record)">查看</a>
+            <a-divider type="vertical" v-if="role!='student'" />
+            <a @click="goToAppList(record)" v-if="role!='student'">查看申请</a>
+            <a-divider type="vertical" v-if="role!='student'" />
+            <a @click="goToStudentList(record)" v-if="role!='student'">学生管理</a>
           </template>
         </span>
       </a-table>
@@ -55,119 +53,119 @@
 <script>
 /* import moment from 'moment'
 import { getRoleList, getServiceList } from '@/api/manage' */
-import store from '@/store'
-import { getInternshipList } from '@/api/enterprise'
-import { getInternships } from '@/api/student'
-import { dateTransformer } from '@/utils/util'
+import store from "@/store";
+import { getInternshipList } from "@/api/enterprise";
+import { getInternships } from "@/api/student";
+import { dateTransformer } from "@/utils/util";
 
 const statusMap = {
   0: {
-    status: 'default',
-    text: '待审核'
+    status: "default",
+    text: "待审核"
   },
   1: {
-    status: 'processing',
-    text: '审核中'
+    status: "processing",
+    text: "审核中"
   },
   2: {
-    status: 'success',
-    text: '审核通过'
+    status: "success",
+    text: "审核通过"
   },
   3: {
-    status: 'error',
-    text: '审核未通过'
+    status: "error",
+    text: "审核未通过"
   }
-}
+};
 const enterpriseColumns = [
   {
-    title: '#',
-    scopedSlots: { customRender: 'serial' }
+    title: "#",
+    scopedSlots: { customRender: "serial" }
   },
   {
-    title: '实训题目',
-    dataIndex: 'topic'
+    title: "实训题目",
+    dataIndex: "topic"
   },
   {
-    title: '实训开始日期',
-    dataIndex: 'exp_begin_time'
+    title: "实训开始日期",
+    dataIndex: "exp_begin_time"
   },
   {
-    title: '实训结束日期',
-    dataIndex: 'exp_end_time'
+    title: "实训结束日期",
+    dataIndex: "exp_end_time"
   },
   {
-    title: '提交日期',
-    dataIndex: 'submit_time'
+    title: "提交日期",
+    dataIndex: "submit_time"
   },
   {
-    title: '实训描述',
-    dataIndex: 'description',
-    scopedSlots: { customRender: 'description' }
+    title: "实训描述",
+    dataIndex: "description",
+    scopedSlots: { customRender: "description" }
   },
   {
-    title: '意向人数',
-    dataIndex: 'need_num'
+    title: "意向人数",
+    dataIndex: "need_num"
   },
   {
-    title: '申请截至日期',
-    dataIndex: 'apply_end_time'
+    title: "申请截至日期",
+    dataIndex: "apply_end_time"
   },
   {
-    title: '审核状态',
-    dataIndex: 'status',
-    scopedSlots: { customRender: 'status' }
+    title: "审核状态",
+    dataIndex: "status",
+    scopedSlots: { customRender: "status" }
   },
   {
-    title: '操作',
-    dataIndex: 'action',
-    scopedSlots: { customRender: 'action' }
+    title: "操作",
+    dataIndex: "action",
+    scopedSlots: { customRender: "action" }
   }
-]
+];
 const studentColumns = [
   {
-    title: '#',
-    scopedSlots: { customRender: 'serial' }
+    title: "#",
+    scopedSlots: { customRender: "serial" }
   },
-  { title: '公司' },
+  { title: "公司" },
   {
-    title: '实训题目',
-    dataIndex: 'topic'
-  },
-  {
-    title: '实训开始日期',
-    dataIndex: 'exp_begin_time'
+    title: "实训题目",
+    dataIndex: "topic"
   },
   {
-    title: '实训结束日期',
-    dataIndex: 'exp_end_time'
+    title: "实训开始日期",
+    dataIndex: "exp_begin_time"
   },
   {
-    title: '提交日期',
-    dataIndex: 'submit_time'
+    title: "实训结束日期",
+    dataIndex: "exp_end_time"
   },
   {
-    title: '实训描述',
-    dataIndex: 'description',
-    scopedSlots: { customRender: 'description' }
+    title: "提交日期",
+    dataIndex: "submit_time"
   },
   {
-    title: '意向人数',
-    dataIndex: 'need_num'
+    title: "实训描述",
+    dataIndex: "description",
+    scopedSlots: { customRender: "description" }
   },
   {
-    title: '申请截至日期',
-    dataIndex: 'apply_end_time'
+    title: "意向人数",
+    dataIndex: "need_num"
+  },
+  {
+    title: "申请截至日期",
+    dataIndex: "apply_end_time"
   },
 
   {
-    title: '操作',
-    dataIndex: 'action',
-    scopedSlots: { customRender: 'action' }
+    title: "操作",
+    dataIndex: "action",
+    scopedSlots: { customRender: "action" }
   }
-]
+];
 export default {
-  name: 'ExpTableList',
-  data () {
+  name: "ExpTableList",
+  data() {
     return {
       enterpriseColumns,
       studentColumns,
@@ -175,70 +173,72 @@ export default {
       queryParam: {},
       loading: false,
       internships: [],
-      role: ''
-    }
+      role: ""
+    };
   },
-  mounted () {
-    this.role = store.getters.role
-    if (this.role === 'enterprise') {
-      getInternshipList().then((res) => {
-        this.internships = dateTransformer(res.data)
-      })
-    } else if (this.role === 'student') {
-      getInternships().then((res) => {
-        this.internships = res.data
-        this.internships = dateTransformer(res.data)
-      })
+  mounted() {
+    this.role = store.getters.role;
+    if (this.role === "enterprise") {
+      getInternshipList().then(res => {
+        this.internships = dateTransformer(res.data);
+      });
+    } else if (this.role === "student") {
+      getInternships().then(res => {
+        this.internships = res.data;
+        this.internships = dateTransformer(res.data);
+      });
     }
   },
   filters: {
-    statusFilter (type) {
-      return statusMap[type].text
+    statusFilter(type) {
+      return statusMap[type].text;
     },
-    statusTypeFilter (type) {
-      return statusMap[type].status
+    statusTypeFilter(type) {
+      return statusMap[type].status;
     },
-    ellipsis (value) {
-      if (!value) return ''
+    ellipsis(value) {
+      if (!value) return "";
       if (value.length > 4) {
-        return value.slice(0,4) + '...'
+        return value.slice(0, 4) + "...";
       }
-      return value
+      return value;
     }
   },
-  methods: { 
-  goToExpDetail (e) {
-    const _this = this
-    console.log(e)
-    store.commit('SET_EXP_ID', e._id)
-    _this.$router.push({ name: 'internshipdetail' })
-  },
-  goToAppList(e) {
-    const _this = this
-    console.log(e)
-    store.commit('SET_EXP_ID', e._id)
-    _this.$router.push({ name: 'applicatinlist' })
-  },
-  goToStudentList(e) {
-    const _this = this
-    console.log(e)
-    store.commit('SET_EXP_ID', e._id)
-    _this.$router.push({ name: 'studentmanage' })
-  },
-  columnsSelector () {
-    this.role = store.getters.role
-    if (this.role === 'enterprise') {
-      return this.enterpriseColumns
-    } else if (this.role === 'student') {
-      return this.studentColumns
-    } else { return this.adminColums }
-  },
-  Searchlist () {
-    this.loading = true
-    setTimeout(() => {
-      this.loading = false
-    }, 300)
+  methods: {
+    goToExpDetail(e) {
+      const _this = this;
+      console.log(e);
+      store.commit("SET_EXP_ID", e._id);
+      _this.$router.push({ name: "internshipdetail" });
+    },
+    goToAppList(e) {
+      const _this = this;
+      console.log(e);
+      store.commit("SET_EXP_ID", e._id);
+      _this.$router.push({ name: "applicatinlist" });
+    },
+    goToStudentList(e) {
+      const _this = this;
+      console.log(e);
+      store.commit("SET_EXP_ID", e._id);
+      _this.$router.push({ name: "studentmanage" });
+    },
+    columnsSelector() {
+      this.role = store.getters.role;
+      if (this.role === "enterprise") {
+        return this.enterpriseColumns;
+      } else if (this.role === "student") {
+        return this.studentColumns;
+      } else {
+        return this.adminColums;
+      }
+    },
+    Searchlist() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 300);
+    }
   }
-  }
-}
+};
 </script>
